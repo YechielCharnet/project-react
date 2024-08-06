1cdf5f671863d999adcbc278210b9cf7a1489d38
YechielCharnet <yechiel1995@gmail.com>
2024-08-06 18:22:40 +0300

    update keyboard

--- project/src/App.js
+++ project/src/App.js
@@ -1,10 +1,10 @@
-import './App.css';
-
+import "./App.css";
+import Keyboard from "./components/Keyboard";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <Keyboard />
       </header>
     </div>
   );
